b231ea4442976bb6a88c76811c1eb93bcf32d53d
westurner <@westurner>
2025-04-27 16:33:26 +0000

    DOC: entrypoint.sh: document what an ENTRYPOINT script in a Dockerfile is

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+
+## container entrypoint script
+# This script is executed when the container starts
+# if ENTRYPOINT specifies a path to this script in the Dockerfile.
+# It sets up the environment and executes the provided command and arguments.
+
 set -x
 entrypoint_sh_args="${@}"
 source /.env >&2
